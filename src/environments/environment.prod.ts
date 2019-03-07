00c43dd863efeaab26d7a058de5899169df64c59
PRATHYUSHA <prathyusha2125>
2019-03-07 12:29:31 +0530

    hii hello

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAQycHyndGz451OBMNSRkH4JQvUYpbtZeE',
+    authDomain: 'silicon-talent-233608.firebaseapp.com',
+    databaseURL: 'https://silicon-talent-233608.firebaseio.com',
+    projectId: 'silicon-talent-233608',
+    storageBucket: 'silicon-talent-233608.appspot.com',
+    messagingSenderId: '889286771743'
+  }
+};
